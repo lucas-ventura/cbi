519c74422192cfe4c9617b65672d59d120260095
lucasventurar <lucas.ventura.r@gmail.com>
2018-12-11 15:39:02 +0100

    send i receive

---
+++ receive.py
@@ -0,0 +1,12 @@
+import socket
+UDP_IP = "128.141.118.161"
+UDP_PORT = 5005
+
+while True:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(("", UDP_PORT))
+    data, addr = sock.recvfrom(1024)
+    print "received message: ", data
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto((raw_input('Message: ')).encode(), (UDP_IP, UDP_PORT))
