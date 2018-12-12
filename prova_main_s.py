5e8cd3187d514440a81a3949594261caaf1e1cfb
lucasventurar <lucas.ventura.r@gmail.com>
2018-12-12 22:22:38 +0100

    a

--- prova_main_s.py
+++ prova_main_s.py
@@ -111,8 +111,8 @@ if __name__ == '__main__':
 
         ## CORRECT ANSWER
         both = resposta + data
-        imageBlack = Image.open('bmp/' + chooseImage(both)) # Demo exercise
         print(both)
+        imageBlack = Image.open('bmp/' + chooseImage(both)) # Demo exercise
         imageRed = Image.open('bmp/blank.bmp')
         epd.display(epd.getbuffer(imageBlack),epd.getbuffer(imageRed))
 
