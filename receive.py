2de2eaa2abd36f03e0ac168c1f1cd017ebb55005
lucasventurar <lucas.ventura.r@gmail.com>
2018-12-12 20:46:00 +0100

    while ip

--- receive.py
+++ receive.py
@@ -1,12 +1,10 @@
 import socket
 UDP_IP = "128.141.118.251"
 UDP_PORT = 5005
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(("", UDP_PORT))
-
-data, addr = sock.recvfrom(1024)
-print "received message: ", data
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto((raw_input('Message: ')).encode(), (UDP_IP, UDP_PORT))
+ while True:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(("", UDP_PORT))
+    data, addr = sock.recvfrom(1024)
+    print "received message: ", data
+     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto((raw_input('Message: ')).encode(), (UDP_IP, UDP_PORT))
--- send.py
+++ send.py
@@ -1,14 +1,10 @@
 import socket
-
 UDP_IP = "128.141.118.75" #IP de l'altre
 UDP_PORT = 5005
-
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto((raw_input('Message: ')).encode(), (UDP_IP, UDP_PORT))
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(("", UDP_PORT))
-
-data, addr = socket.recvfrom(1024)
-print "received message: ", data
+ while True:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto((raw_input('Message: ')).encode(), (UDP_IP, UDP_PORT))
+     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind("", UDP_PORT)
+     data, addr = socket.recvfrom(1024)
+    print "received message: ", data
