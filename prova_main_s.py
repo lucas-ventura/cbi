f641a37deecc130799f1464e62d30d9662815648
lucasventurar <lucas.ventura.r@gmail.com>
2018-12-12 19:35:12 +0100

    asdf

--- prova_main_s.py
+++ prova_main_s.py
@@ -77,7 +77,7 @@ if __name__ == '__main__':
 
 
         # DEMO EXERCISE
-        x=0
+        #x=0
 
         imageBlack = Image.open('bmp/' + chooseImage('2')) # Demo exercise
         imageRed = Image.open('bmp/blank.bmp')
