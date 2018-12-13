87aa31f4fbb75def97da4acded10259f535ff912
lucasventurar <lucas.ventura.r@gmail.com>
2018-12-13 12:38:21 +0100

    j

--- prova_main_r.py
+++ prova_main_r.py
@@ -119,6 +119,7 @@ if __name__ == '__main__':
         epd.display(epd.getbuffer(imageBlack),epd.getbuffer(imageRed))
         playMusic('mp3/' + '3' + '.wav')
 
+        x=0
         while x==0:
             print(x)
 
